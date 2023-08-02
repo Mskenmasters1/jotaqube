0cc1211ca41f26bade0867fedda31d2e6c848b1c
Quico Saval <quicosv@gmail.com>
2023-08-02 19:12:50 +0200

    Convierte los espacios a tabs en los archivos recién añadidos

--- frontend/src/pages/codigo/CodigoForm.tsx
+++ frontend/src/pages/codigo/CodigoForm.tsx
@@ -7,64 +7,64 @@ import { clienteAxios } from '../../config/clienteAxios';
 import { IMensaje } from '../../interfaces/mensaje.interface';
 
 interface IChatFormProps {
-  idSala: number;
-  sala: string;
+	idSala: number;
+	sala: string;
 }
 
 export const ChatForm = ({ idSala, sala }: IChatFormProps) => {
-  const { usuarioInfo } = useContext<IUsuarioInfoContext>(AppContext);
-  const [errorMsg, setErrorMsg] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(false);
+	const { usuarioInfo } = useContext<IUsuarioInfoContext>(AppContext);
+	const [errorMsg, setErrorMsg] = useState<string>('');
+	const [loading, setLoading] = useState<boolean>(false);
 
-  const { form, onInputChange } = useForm<IMensaje>({
-    usuarios_email: usuarioInfo.email,
-    texto: '',
-    salas_idSala: idSala
-  });
+	const { form, onInputChange } = useForm<IMensaje>({
+		usuarios_email: usuarioInfo.email,
+		texto: '',
+		salas_idSala: idSala
+	});
 
-  const { usuarios_email, texto, salas_idSala } = form;
+	const { usuarios_email, texto, salas_idSala } = form;
 
-  const send = async (e: FormEvent) => {
-    e.preventDefault();
+	const send = async (e: FormEvent) => {
+		e.preventDefault();
 
-    try {
-      setLoading(true);
-      setErrorMsg('');
-      await clienteAxios.post<IMensaje>('/mensajes', { usuarios_email, texto, salas_idSala, sala });
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      const errores = await handlerAxiosError(error);
-      setErrorMsg(errores);
-    }
-  };
+		try {
+			setLoading(true);
+			setErrorMsg('');
+			await clienteAxios.post<IMensaje>('/mensajes', { usuarios_email, texto, salas_idSala, sala });
+			setLoading(false);
+		} catch (error) {
+			setLoading(false);
+			const errores = await handlerAxiosError(error);
+			setErrorMsg(errores);
+		}
+	};
 
-  return (
-    <>
-      <h2>Enviar mensaje</h2>
-      <hr />
+	return (
+		<>
+			<h2>Enviar mensaje</h2>
+			<hr />
 
-      <form onSubmit={send}>
-        <div className="form-group">
-          <label htmlFor="texto">Texto</label>
-          <input id="texto" type="texto" className="form-control" value={texto} onChange={onInputChange} required />
-        </div>
+			<form onSubmit={send}>
+				<div className="form-group">
+					<label htmlFor="texto">Texto</label>
+					<input id="texto" type="texto" className="form-control" value={texto} onChange={onInputChange} required />
+				</div>
 
-        <button className="btn btn-success" type="submit" disabled={texto.trim() === ''}>
-          Enviar
-        </button>
-      </form>
-      {loading && (
-        <div className="alert alert-warning" role="alert">
-          Enviando mensaje...
-        </div>
-      )}
-      {/* Si errorFetch es true, mostramos un mensaje de error al usuario */}
-      {errorMsg && !loading && (
-        <div className="alert alert-danger" role="alert">
-          {errorMsg}
-        </div>
-      )}
-    </>
-  );
+				<button className="btn btn-success" type="submit" disabled={texto.trim() === ''}>
+					Enviar
+				</button>
+			</form>
+			{loading && (
+				<div className="alert alert-warning" role="alert">
+					Enviando mensaje...
+				</div>
+			)}
+			{/* Si errorFetch es true, mostramos un mensaje de error al usuario */}
+			{errorMsg && !loading && (
+				<div className="alert alert-danger" role="alert">
+					{errorMsg}
+				</div>
+			)}
+		</>
+	);
 };
--- frontend/src/pages/codigo/CodigoList.tsx
+++ frontend/src/pages/codigo/CodigoList.tsx
@@ -6,69 +6,69 @@ import { clienteAxios } from '../../config/clienteAxios';
 import { handlerAxiosError } from '../../helpers/handlerAxiosError';
 
 interface IChatMensajesListProps {
-  idSala: number;
+	idSala: number;
 }
 
 export const ChatMensajesList = ({ idSala }: IChatMensajesListProps) => {
-  const { usuarioInfo } = useContext<IUsuarioInfoContext>(AppContext);
-  const [mensajes, setMensajes] = useState<IMensaje[]>([]);
-  const [errorMsg, setErrorMsg] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(false);
-  const [ok, setOk] = useState<boolean>(true);
-  const { socket } = usuarioInfo;
-  socket?.on('mensajes-sala', (mensajes: IMensaje[]) => {
-    setMensajes(mensajes);
-  });
+	const { usuarioInfo } = useContext<IUsuarioInfoContext>(AppContext);
+	const [mensajes, setMensajes] = useState<IMensaje[]>([]);
+	const [errorMsg, setErrorMsg] = useState<string>('');
+	const [loading, setLoading] = useState<boolean>(false);
+	const [ok, setOk] = useState<boolean>(true);
+	const { socket } = usuarioInfo;
+	socket?.on('mensajes-sala', (mensajes: IMensaje[]) => {
+		setMensajes(mensajes);
+	});
 
-  useEffect(() => {
-    getMensajes();
-  }, []);
+	useEffect(() => {
+		getMensajes();
+	}, []);
 
-  const getMensajes = async () => {
-    try {
-      setLoading(true);
-      setErrorMsg('');
-      const { data } = await clienteAxios.get<IMensaje[]>(`/mensajes/${idSala}`);
-      setMensajes(data);
-      setLoading(false);
-      setOk(true);
-    } catch (error) {
-      setOk(false);
-      setLoading(false);
-      const errores = await handlerAxiosError(error);
-      setErrorMsg(errores);
-    }
-  };
+	const getMensajes = async () => {
+		try {
+			setLoading(true);
+			setErrorMsg('');
+			const { data } = await clienteAxios.get<IMensaje[]>(`/mensajes/${idSala}`);
+			setMensajes(data);
+			setLoading(false);
+			setOk(true);
+		} catch (error) {
+			setOk(false);
+			setLoading(false);
+			const errores = await handlerAxiosError(error);
+			setErrorMsg(errores);
+		}
+	};
 
-  const transformDate = (fechaHora: Date) => {
-    return `${padL(fechaHora.getMonth() + 1)}/${padL(fechaHora.getDate())}/${fechaHora.getFullYear()} ${padL(
-      fechaHora.getHours()
-    )}:${padL(fechaHora.getMinutes())}:${padL(fechaHora.getSeconds())}
-    `;
-  };
+	const transformDate = (fechaHora: Date) => {
+		return `${padL(fechaHora.getMonth() + 1)}/${padL(fechaHora.getDate())}/${fechaHora.getFullYear()} ${padL(
+			fechaHora.getHours()
+		)}:${padL(fechaHora.getMinutes())}:${padL(fechaHora.getSeconds())}
+		`;
+	};
 
-  const padL = (nr: number, len = 2, chr = `0`) => `${nr}`.padStart(len, chr);
+	const padL = (nr: number, len = 2, chr = `0`) => `${nr}`.padStart(len, chr);
 
-  return (
-    <>
-      <h2>Mensajes</h2>
-      <ul className="list-group">
-        {mensajes.map((x) => (
-          <li className="list-group-item" key={x.idMensaje}>
-            {x.usuarios_email} - {transformDate(new Date(x.fechaHora!))}: {x.texto}
-          </li>
-        ))}
-      </ul>
-      {loading && (
-        <div className="alert alert-warning" role="status" aria-live="polite">
-          Actualizando mensajes...
-        </div>
-      )}
-      {!ok && errorMsg && (
-        <div className="alert alert-danger" role="status" aria-live="polite">
-          {errorMsg}
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			<h2>Mensajes</h2>
+			<ul className="list-group">
+				{mensajes.map((x) => (
+					<li className="list-group-item" key={x.idMensaje}>
+						{x.usuarios_email} - {transformDate(new Date(x.fechaHora!))}: {x.texto}
+					</li>
+				))}
+			</ul>
+			{loading && (
+				<div className="alert alert-warning" role="status" aria-live="polite">
+					Actualizando mensajes...
+				</div>
+			)}
+			{!ok && errorMsg && (
+				<div className="alert alert-danger" role="status" aria-live="polite">
+					{errorMsg}
+				</div>
+			)}
+		</>
+	);
 };
